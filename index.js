23e5b07c67a87ad1457e9c01ba72c22aa6b8be9c
arms1997 <armeen.hadizadeh@gmail.com>
2021-01-10 00:00:50 +0000

    created a recurssive function to handle an array of numbers

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+const sum = require("../sum-from-a-to-b-test-js");
+
 function sumItems(array) {
   // Sum all the numbers in the array
+  let accumulator = 0;
+  array.forEach((value)=> {
+    if(Array.isArray(value)){
+      accumulator += sumItems(value)
+    }else{
+      accumulator += value
+    }
+    
+  })
+
+  return accumulator
 }
 
 module.exports = sumItems;
